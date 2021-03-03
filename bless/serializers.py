3abc12c38e86299cb4fb5cdb6128ac2a0bc75466
jcanete <joachimcanete@gmail.com>
2021-03-03 10:48:19 -0600

    finalized serializers

--- bless/serializers.py
+++ bless/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import Blessing, Comment
 
-class BlessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blessing
-        fields = (
-            'id',
-            'author', 
-            'title', 
-            'content'
-        )
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
@@ -19,4 +9,16 @@ class CommentSerializer(serializers.ModelSerializer):
             'commenter',
             'blessing',
             'content',
+        )
+
+class BlessSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer (many = True, read_only = True)
+    class Meta:
+        model = Blessing
+        fields = (
+            'id',
+            'author', 
+            'title', 
+            'content',
+            'comments',
         )
